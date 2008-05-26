f4f6ba41705646124abb496121372143ec72a891
Dr Nic Williams <drnicwilliams@gmail.com>
2008-05-26 14:35:18 +1000

    Show in GitHub now working

--- Support/bin/show_in_github.rb
+++ Support/bin/show_in_github.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 
 $:.unshift(File.dirname(__FILE__) + "/../lib")
+require "rubygems"
 require "show_in_github"
 
 url = ShowInGitHub.url_for(ENV['TM_FILEPATH'])
 if url
-  `open #{url}#L#{TM_LINE_NUMBER}`
+  `open #{url}#L#{ENV['TM_LINE_NUMBER']}`
 else
   puts "File/project not a git repository or not pushed to a github repository"
 end
