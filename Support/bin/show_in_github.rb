a69becb746d18f5ad829fd4763d557431ffe52a4
Dr Nic Williams <drnicwilliams@gmail.com>
2008-05-26 14:42:18 +1000

    if multiple lines are selected, then the 'Show in GitHub' highlights all lines

--- Support/bin/show_in_github.rb
+++ Support/bin/show_in_github.rb
@@ -6,7 +6,8 @@ require "show_in_github"
 
 url = ShowInGitHub.url_for(ENV['TM_FILEPATH'])
 if url
-  `open #{url}#L#{ENV['TM_LINE_NUMBER']}`
+  lines = ENV['TM_INPUT_START_LINE'] ? "#{ENV['TM_INPUT_START_LINE']}-#{ENV['TM_LINE_NUMBER'].to_i - 1}" : ENV['TM_LINE_NUMBER']
+  `open #{url}#L#{lines}`
 else
   puts "File/project not a git repository or not pushed to a github repository"
 end
