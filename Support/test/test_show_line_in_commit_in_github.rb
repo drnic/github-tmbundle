3becfbcb01574cb4efbcc553ad4be37f6e428e03
Dr Nic Williams <drnicwilliams@gmail.com>
2008-05-28 08:22:31 +1000

    need a commit to look for; so it will be this one

---
+++ Support/test/test_show_line_in_commit_in_github.rb
@@ -0,0 +1,8 @@
+require File.dirname(__FILE__) + "/test_helper"
+require "show_in_github"
+
+class TestShowLineInCommitInGitHub < Test::Unit::TestCase
+  def test_find_first_commit_for_this_file
+    
+  end
+end
