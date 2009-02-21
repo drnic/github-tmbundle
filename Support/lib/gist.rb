c4f6d3e23964f6848e2bdc0312767e5c6860b96e
Jari Bakken <jari.bakken@gmail.com>
2009-02-21 18:23:18 +0100

    Make sure gists creation still works when TM_PROJECT_DIRECTORY is nil.

--- Support/lib/gist.rb
+++ Support/lib/gist.rb
@@ -44,7 +44,7 @@ module Gist
       gistname = "snippet" << "." << get_extension
     else
       selection = STDIN.read
-      gistname = ENV['TM_FILEPATH'] ? ENV['TM_FILEPATH'].sub(ENV['TM_PROJECT_DIRECTORY'], '') : "file" << "." << get_extension
+      gistname = ENV['TM_FILEPATH'] ? ENV['TM_FILEPATH'].sub(ENV['TM_PROJECT_DIRECTORY'].to_s, '') : "file" << "." << get_extension
     end
     
     add_file(gistname, selection)
